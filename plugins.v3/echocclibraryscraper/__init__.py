119ecb7d022a9146b3e9f19fa9edc07cec08a5a4
AisenCode <58407485+AisenCode@users.noreply.github.com>
2026-09-17 23:11:01 +0800

    Update __init__.py

--- plugins.v3/echocclibraryscraper/__init__.py
+++ plugins.v3/echocclibraryscraper/__init__.py
@@ -26,7 +26,7 @@ from sqlalchemy.orm import DeclarativeBase
 
 class EchoCCLibraryScraper(_PluginBase):
     # 插件名称
-    plugin_name = "媒体库刮削"
+    plugin_name = "媒体库刮削专享版"
     # 插件描述
     plugin_desc = "定时对媒体库进行刮削，补齐缺失元数据和图片。"
     # 插件图标
